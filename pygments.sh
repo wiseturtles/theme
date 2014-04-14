84c9aa7df1fe9108067b0d16808260ecb2ec0eea
linliang <lianglin999@gmail.com>
2014-04-14 23:36:06 +0800

    add pygments css file

--- pygments.sh
+++ pygments.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # 这个脚本用于生成自己感觉不错的一些主题的css文件
 
-themes="friendly pastie emacs bw trac murphy default colorful borland perldoc"
+themes="default emacs friendly colorful autumn murphy manni monokai perldoc pastie borland trac native fruity bw vs tango"
 OUT_DIR="static/pygments"
 if [ ! -d $OUT_DIR ]; then
     mkdir -p $OUT_DIR
